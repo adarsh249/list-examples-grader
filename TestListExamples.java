478dff47de3c5dc051864746fd829e108f870159
adarsh249 <adarshpatel249@gmail.com>
2022-11-15 22:07:35 -0800

    edits

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,50 @@
+import org.junit.Test;
+import java.util.List;
 import static org.junit.Assert.*;
-import org.junit.*;
+import java.util.ArrayList;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void filterTest(){
+    StringChecker sc = new StringChecker() {
+            
+      public boolean checkString(String s){
+          return s.length() < 8;
+      }
+  };
+
+  ArrayList<String> list = new ArrayList<>();
+  list.add("Pineapple");
+  list.add("Apple");
+  list.add("Orange");
+
+  List<String> newList = ListExamples.filter(list, sc);
+
+  ArrayList<String> expected = new ArrayList<>();
+  expected.add("Apple");
+  expected.add("Orange");
+
+  
+  assertArrayEquals(expected.toArray(), newList.toArray());
+  }
+
+  @Test
+  public void mergeTest(){
+    List<String> list1 = new ArrayList<>();
+
+    list1.add("Pineapple");
+    list1.add("Apple");
+
+    List<String> list2 = new ArrayList<>();
+    list2.add("Orange");
+
+    List<String> newList = ListExamples.merge(list1, list2);
+    List<String> expected = new ArrayList<>();
+    expected.add("Apple");
+    expected.add("Orange");
+    expected.add("Pineapple");
+
+    assertArrayEquals(expected.toArray(), newList.toArray());
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,31 @@
 # Create your grading script here
-
-set -e
-
 rm -rf student-submission
 git clone $1 student-submission
 
+echo "Cloning..."
+
+cd student-submission
+
+if [ -f ListExamples.java ]
+then
+    echo "Found student's file."
+else
+    echo "Student file not found."
+    exit
+fi
+
+cp ListExamples.java ..
+
+cd ..
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+if [ $? -ne 0 ]
+then
+    echo "Compiler Error"
+    exit
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > grade.txt
+
+grep -w "run:"
